6550d3d07a9cc302ef4de77e9f6c79564866d108
hanjoohyung <ghd167@naver.com>
2021-11-01 10:06:09 +0900

    asfd

--- jblog03/src/main/java/com/douzone/jblog/controller/MainController.java
+++ jblog03/src/main/java/com/douzone/jblog/controller/MainController.java
@@ -1,8 +1,19 @@
 package com.douzone.jblog.controller;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+	@Autowired
+	ServletContext servletContext;
+	
+	@RequestMapping({"", "/main"})
+	public String index() {
 
+		return "main/index";
+	}
 }
